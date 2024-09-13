9e3e332a1184eb714c741e3f4b65bfb998a98adc
Onurhan Ak <onurhanak@protonmail.com>
2024-09-13 16:24:04 -0400

    added notifications

--- util/config/readConfigFile.go
+++ util/config/readConfigFile.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"AletheiaDesktop/util/shared"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -12,19 +13,22 @@ func ReadConfigFile() (map[string]string, error) {
 	configPath, configPathConstructionErr := ConstructConfigLocation()
 
 	if configPathConstructionErr != nil {
-		log.Fatalln(configPathConstructionErr.Error())
+
+		log.Println(configPathConstructionErr.Error())
 	}
 
 	userConfigContent := map[string]string{}
 	file, readFileErr := os.ReadFile(configPath)
 	if readFileErr != nil {
-		log.Fatalln(fmt.Errorf("Read config file error: %v", readFileErr))
+		shared.SendNotification("Error", "Cannot read your config file. Please delete aletheia.cfg and start over.")
+		log.Println(fmt.Errorf("Read config file error: %v", readFileErr))
 		return nil, readFileErr
 	}
 
 	unmarshalErr := json.Unmarshal(file, &userConfigContent)
 	if unmarshalErr != nil {
-		log.Fatalln(fmt.Errorf("Unmarshal config file error: %v", unmarshalErr))
+		shared.SendNotification("Error", "Cannot read your config file. Please delete aletheia.cfg and start over.")
+		log.Println(fmt.Errorf("Unmarshal config file error: %v", unmarshalErr))
 		return nil, unmarshalErr
 	}
 	return userConfigContent, nil
